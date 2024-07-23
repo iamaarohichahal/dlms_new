abf536ec4c16afcaba70080f52443cb73740105a
iamaarohichahal <iamaarohichahal@gmail.com>
2024-07-23 13:57:39 +0800

    initial version
    initial version

---
+++ login.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
